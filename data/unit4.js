8d94c87f3568e5f791dbcc51b5a713802e9495c9
leofacelin <leoface@gmail.com>
2025-12-14 19:05:36 +0800

    Add files via upload

---
+++ data/unit4.js
@@ -0,0 +1,37 @@
+const unit4_data = [
+    {
+        word: "cap",
+        meaning: "帽子 (鸭舌帽)",
+        image: "🧢",
+        sentence: "He is wearing a blue _____.",
+        options: ["cap", "cup", "cat"]
+    },
+    {
+        word: "worry",
+        meaning: "担心",
+        image: "😟",
+        sentence: "Don't _____, everything will be fine.",
+        options: ["worry", "sorry", "happy"]
+    },
+    {
+        word: "on",
+        meaning: "在...上面",
+        image: "🔛",
+        sentence: "The apple is _____ the table.",
+        options: ["on", "in", "at"]
+    },
+    {
+        word: "photo",
+        meaning: "照片",
+        image: "📷",
+        sentence: "Let's take a _____ together.",
+        options: ["photo", "phone", "potato"]
+    },
+    {
+        word: "love",
+        meaning: "爱；喜爱",
+        image: "❤️",
+        sentence: "I _____ ice cream very much.",
+        options: ["love", "live", "long"]
+    }
+];
