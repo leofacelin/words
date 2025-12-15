816b0d4e2bcf39f8a68b57b6dd96a1dfa7e79427
leofacelin <leoface@gmail.com>
2025-12-16 07:31:29 +0800

    Add files via upload

---
+++ data/unit5.js
@@ -0,0 +1,37 @@
+const unit5_data = [
+    {
+        word: "daddy",
+        meaning: "爸爸 (亲昵叫法)",
+        image: "👨",
+        sentence: "My _____ plays football with me.",
+        options: ["daddy", "baby", "body"]
+    },
+    {
+        word: "mummy",
+        meaning: "妈妈 (亲昵叫法)",
+        image: "👩",
+        sentence: "_____ gives me a hug.",
+        options: ["mummy", "money", "many"]
+    },
+    {
+        word: "where",
+        meaning: "哪里",
+        image: "❓",
+        sentence: "_____ is my little dog?",
+        options: ["where", "what", "who"]
+    },
+    {
+        word: "dog",
+        meaning: "狗",
+        image: "🐶",
+        sentence: "The _____ says woof woof.",
+        options: ["dog", "bag", "pig"]
+    },
+    {
+        word: "box",
+        meaning: "盒子；箱子",
+        image: "📦",
+        sentence: "Open the _____, please.",
+        options: ["box", "fox", "boy"]
+    }
+];
