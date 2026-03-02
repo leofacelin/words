1df28de7b242673e58caaea27774b103b5d32c98
leofacelin <leoface@gmail.com>
2026-03-02 20:09:14 +0800

    Add files via upload

---
+++ data/unit6.js
@@ -0,0 +1,51 @@
+const unit6_data = [
+    {
+        word: "animal",
+        meaning: "动物",
+        image: "🦁",
+        sentence: "A lion is a big _____.",
+        options: ["animal", "apple", "angry"]
+    },
+    {
+        word: "panda",
+        meaning: "熊猫",
+        image: "🐼",
+        sentence: "The _____ likes to eat bamboo.",
+        options: ["panda", "pencil", "paper"]
+    },
+    {
+        word: "elephant",
+        meaning: "大象",
+        image: "🐘",
+        sentence: "The _____ has a long nose.",
+        options: ["elephant", "egg", "eight"]
+    },
+    {
+        word: "bear",
+        meaning: "熊",
+        image: "🐻",
+        sentence: "The brown _____ is sleeping.",
+        options: ["bear", "pear", "beer"]
+    },
+    {
+        word: "giraffe",
+        meaning: "长颈鹿",
+        image: "🦒",
+        sentence: "A _____ has a very long neck.",
+        options: ["giraffe", "grape", "green"]
+    },
+    {
+        word: "monkey",
+        meaning: "猴子",
+        image: "🐒",
+        sentence: "The _____ climbs the tree.",
+        options: ["monkey", "money", "mouth"]
+    },
+    {
+        word: "them",
+        meaning: "他们；它们 (宾格)",
+        image: "👥",
+        sentence: "I like animals. I like _____.",
+        options: ["them", "then", "they"]
+    }
+];
