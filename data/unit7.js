a06213426bcb853b10d7eae35dc036a70888e890
leofacelin <leoface@gmail.com>
2026-03-03 10:42:31 +0800

    Add files via upload

---
+++ data/unit7.js
@@ -0,0 +1,58 @@
+const unit7_data = [
+    {
+        word: "draw",
+        meaning: "画",
+        image: "🎨",
+        sentence: "I can _____ a big house.",
+        options: ["draw", "drink", "dress"]
+    },
+    {
+        word: "tiger",
+        meaning: "老虎",
+        image: "🐯",
+        sentence: "The _____ is very fast.",
+        options: ["tiger", "table", "teacher"]
+    },
+    {
+        word: "strong",
+        meaning: "强壮的",
+        image: "💪",
+        sentence: "The lion is very _____.",
+        options: ["strong", "story", "string"]
+    },
+    {
+        word: "short",
+        meaning: "矮的；短的",
+        image: "📏",
+        sentence: "The rabbit has a _____ tail.",
+        options: ["short", "shirt", "shoes"]
+    },
+    {
+        word: "white",
+        meaning: "白色",
+        image: "⚪",
+        sentence: "The snow is _____.",
+        options: ["white", "water", "where"]
+    },
+    {
+        word: "brown",
+        meaning: "棕色",
+        image: "🟤",
+        sentence: "The bear is _____.",
+        options: ["brown", "bread", "broom"]
+    },
+    {
+        word: "tall",
+        meaning: "高的",
+        image: "🦒",
+        sentence: "The giraffe is very _____.",
+        options: ["tall", "ball", "tell"]
+    },
+    {
+        word: "him",
+        meaning: "他 (宾格)",
+        image: "👦",
+        sentence: "I know the boy. I like _____.",
+        options: ["him", "his", "her"]
+    }
+];
